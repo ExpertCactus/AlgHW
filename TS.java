93c3cad7279d000d904ee4d3cb8ef803c7d107c3
Markus Shaw <fake.markus@gmail.com>
2015-10-20 20:27:40 -0700

    adding more stuff to get started

--- TS.java
+++ TS.java
@@ -9,11 +9,33 @@ package AlgHW;
  * Date: 
  */
 import java.util.Scanner;
-import java.io.File;
+import java.io.*;
 public class TS {
 
 	public static void main(String[] args) {
+		int numNodes;
+		String filename;
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Input file name: ");
+		filename = keyboard.nextLine();
+		FileReader freader;
+		BufferedReader buffReader = null;
+		try {
+			 freader = new FileReader(filename);
+			 buffReader = new BufferedReader(freader);
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
 		
+		try {
+			numNodes = buffReader.read();
+			System.out.println("num nodes: " + numNodes);//returns 51 when it should be 3
+			buffReader.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
