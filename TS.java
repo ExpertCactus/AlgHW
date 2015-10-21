9a28eb967b257cf8b6e222644479c71d6d2921b1
Markus Shaw <mashaw@csumb.edu>
2015-10-20 19:47:15 -0700

    Getting Started

---
+++ TS.java
@@ -0,0 +1,19 @@
+package AlgHW;
+
+/*
+ * Title: TS.java
+ * Abstract:
+ * 
+ * Name: Markus Shaw
+ * ID: 0893
+ * Date: 
+ */
+
+public class TS {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
